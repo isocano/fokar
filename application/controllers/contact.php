0f305735227f17f3831e74f01f9f8a5e43b44bd6
pvm23 <pablo_velasco_m@hotmail.com>
2012-04-12 21:50:29 +0200

    Conctact view done

---
+++ application/controllers/contact.php
@@ -0,0 +1,21 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Contact extends CI_Controller {
+	
+	
+	public function index()
+	{
+		$data_head['title'] = 'Fokar | Contacto';
+		$data_head['description'] = 'Sigue a personas con karma en base al sistema de karmacracy.';
+		
+		$this->load->view('templates/head.php', $data_head);
+		$this->load->view('templates/header.php');
+		
+		$this->load->view('contact_view');
+		
+		$this->load->view('templates/footer.php');
+	}
+}
+
+/* End of file contact.php */
+/* Location: ./application/controllers/contact.php */
---
+++ application/views/contact_view.php
@@ -0,0 +1,95 @@
+<script src="http://max.jotfor.ms/min/g=jotform?3.0.3159" type="text/javascript"></script>
+<script type="text/javascript">
+   JotForm.init(function(){
+      $('input_16').hint('ex: myname@example.com');
+   });
+</script>
+<link href="http://max.jotfor.ms/min/g=formCss?3.0.3159" rel="stylesheet" type="text/css" />
+<style type="text/css">
+    .form-label{
+        width:150px !important;
+    }
+    .form-label-left{
+        width:150px !important;
+    }
+    .form-line{
+        padding:10px;
+    }
+    .form-label-right{
+        width:150px !important;
+    }
+    .form-all{
+        width:650px;
+        color:Black !important;
+        font-family:'Verdana';
+        font-size:12px;
+    }
+</style>
+
+<form class="jotform-form" action="http://submit.jotformeu.com/submit/21025681811345/" method="post" name="form_21025681811345" id="21025681811345" accept-charset="utf-8">
+  <input type="hidden" name="formID" value="21025681811345" />
+  <div class="form-all">
+    <ul class="form-section">
+      <li id="cid_18" class="form-input-wide">
+        <div class="form-header-group">
+          <h2 id="header_18" class="form-header">
+            Contacto
+          </h2>
+          <div id="subHeader_18" class="form-subHeader">
+            Por favor, rellene este formulario y nos pondremos en contacto con usted en breve.
+          </div>
+        </div>
+      </li>
+      <li class="form-line" id="id_20">
+        <label class="form-label-top" id="label_20" for="input_20"> Nombre: </label>
+        <div id="cid_20" class="form-input-wide">
+          <input type="text" class="form-textbox" id="input_20" name="q20_nombre20" size="20" />
+        </div>
+      </li>
+      <li class="form-line" id="id_16">
+        <label class="form-label-top" id="label_16" for="input_16"> Email: </label>
+        <div id="cid_16" class="form-input-wide">
+          <input type="email" class="form-textbox validate[Email]" id="input_16" name="q16_email" size="20" />
+        </div>
+      </li>
+      <li class="form-line" id="id_17">
+        <label class="form-label-top" id="label_17" for="input_17"> Su mensaje: </label>
+        <div id="cid_17" class="form-input-wide">
+          <textarea id="input_17" class="form-textarea" name="q17_suMensaje" cols="60" rows="9"></textarea>
+        </div>
+      </li>
+      <li class="form-line" id="id_19">
+        <label class="form-label-top" id="label_19" for="input_19">
+          Enter the message as it's shown<span class="form-required">*</span>
+        </label>
+        <div id="cid_19" class="form-input-wide">
+          <script type="text/javascript" src="http://www.google.com/recaptcha/api/js/recaptcha_ajax.js"></script>
+          <div id="recaptcha_input_19">
+          </div>
+          <script type="text/javascript">
+          Recaptcha.create("6Ld9UAgAAAAAAMon8zjt30tEZiGQZ4IIuWXLt1ky", "recaptcha_input_19", {
+  theme: "clean"
+});
+          </script>
+        </div>
+      </li>
+      <li class="form-line" id="id_14">
+        <div id="cid_14" class="form-input-wide">
+          <div style="text-align:center" class="form-buttons-wrapper">
+            <button id="input_14" type="submit" class="form-submit-button">
+              Enviar
+            </button>
+          </div>
+        </div>
+      </li>
+      <li style="display:none">
+        Should be Empty:
+        <input type="text" name="website" value="" />
+      </li>
+    </ul>
+  </div>
+  <input type="hidden" id="simple_spc" name="simple_spc" value="21025681811345" />
+  <script type="text/javascript">
+  document.getElementById("si" + "mple" + "_spc").value = "21025681811345-21025681811345";
+  </script>
+</form>
