5f4deafed0722f59b14950f77e36d6f881e17176
Isocano <dolclaflu@gmail.com>
2012-04-12 20:36:47 +0200

    Estilo header

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,3 +1,20 @@
-	<div id="head">
-		<h3>Fokar, el sitio que más mola</h3>
-	</div>
+	<div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container-fluid">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="#">Fokar, el sitio que más mola</a>
+          <div class="nav-collapse">
+            <ul class="nav">
+              <li class="active"><a href="#">Inicio</a></li>
+              <li><a href="#ranking">Ranking</a></li>
+              <li><a href="#contact">Contacto</a></li>
+            </ul>
+            <p class="navbar-text pull-right">Logged in as <a href="#">username</a></p>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
