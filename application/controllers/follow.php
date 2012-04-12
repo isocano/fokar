e3b5d78fabbf518404165fd79cbc484691598b54
Isocano <dolclaflu@gmail.com>
2012-04-12 17:47:52 +0200

    Base de los controladores

---
+++ application/controllers/follow.php
@@ -0,0 +1,21 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Follow extends CI_Controller 
+{
+	function __construct() 
+	{
+		parent::__construct();
+		
+		//TODO quitar		
+		error_reporting(E_ALL);
+		ini_set("display_errors", 1);
+	}
+	
+	function index() 
+	{
+		
+	}
+}
+
+/* End of file follow.php */
+/* Location: ./controllers/follow.php */
