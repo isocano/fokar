6275c690a5b4e95241c3643277804fd315e70cbf
Isocano <dolclaflu@gmail.com>
2012-04-12 21:02:56 +0200

    Estilo ranking y footer

--- application/views/ranking_view.php
+++ application/views/ranking_view.php
@@ -1,6 +1,51 @@
-<?php
- foreach ($ranking['data']['user'] as $i) {
-     echo "<p>" .$i['username']. "</p>";
- }
- 
-?>
+	
+
+<div class="container-fluid">
+      <div class="row-fluid">
+        <div class="span9">
+          <div class="hero-unit">
+            <h1>Ranking</h1>
+            <p>En fokar nos gustan las listas, aquí están los mejores =)</p>
+            <table class="table table-striped">
+				<thead>
+			    <tr>
+			      <th>Usuario</th>
+			    </tr>
+			  </thead>
+			  <tbody>
+			  	<?php
+					foreach ($ranking['data']['user'] as $i) {
+						echo '<tr>';
+							echo '<td>' .$i['username']. '</td>';
+							echo '<td><a class="btn btn-small btn-info" href="#"><i class="icon-plus icon-white"></i> Seguir</a></td>';
+						echo '</tr>';
+					}
+				?>
+			  </tbody>
+			</table>
+          </div>
+        </div><!--/span-->
+        <div class="span3">
+          <div class="well sidebar-nav">
+            <ul class="nav nav-list">
+              <li class="nav-header">Sidebar</li>
+              <li class="active"><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li class="nav-header">Sidebar</li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li class="nav-header">Sidebar</li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+            </ul>
+          </div><!--/.well -->
+        </div><!--/span-->
+      </div><!--/row-->
+    </div><!--/.fluid-container-->
